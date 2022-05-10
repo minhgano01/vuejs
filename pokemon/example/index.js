d47fc741cdba760e4a494831d0d2b4ce9a5e498d
thoCode <minhgano14@gmail.com>
2022-05-10 18:03:21 +0700

    commit 1

---
+++ pokemon/example/index.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+    data: function()
+     {
+         return{
+            nameUser: "Hà Công Minh",
+            className: "vuejs"
+         }
+     }
+    
+}).mount("#interpolation")
