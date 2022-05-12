40c7ae83f2ce643e264282bb156debe93744e2c6
thoCode <minhgano14@gmail.com>
2022-05-12 15:30:51 +0700

    raw-html

--- pokemon/example/index.js
+++ pokemon/example/index.js
@@ -1,10 +1,45 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 Vue.createApp({
-    data: function()
-     {
-         return{
+    data: function () {
+        return {
             nameUser: "Hà Công Minh",
-            className: "vuejs"
-         }
-     }
-    
-}).mount("#interpolation")
+            className: "vuejs",
+        };
+    },
+}).mount("#interpolation");
+
+Vue.createApp({
+    data() {
+        return {
+            imgURL: "./cat.png",
+            linkCser: "https://cser.vn/",
+        };
+    },
+}).mount("#bind-attribute");
+
+Vue.createApp({
+    methods: {
+        resuflt() {
+            const randomNum = [getRandomInt(1, 20), getRandomInt(1, 20), getRandomInt(1, 20)]
+            const sumNum = randomNum.reduce((tong, diem) => tong = +diem, 0)
+            if (sumNum > 10) {
+                return "tài"
+            } else {
+                return "xỉu"
+            }
+        }
+    }
+}).mount("#methods");
+
+Vue.createApp({
+    data() {
+        return {
+            message: "<h2>Anh Minh thợ code !</h2>"
+        }
+    }
+}).mount("#raw-html")
